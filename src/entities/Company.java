1b5b93c8730c49d6cf202e860442820812cd8d8a
David Magalhães <david.magalhaes90@hotmail.com>
2019-10-07 16:15:01 -0300

    Finalização e implementação

--- src/entities/Company.java
+++ src/entities/Company.java
@@ -1,5 +1,33 @@
 package entities;
 
 public class Company extends TaxPayer{
+	
+	public Integer numberOfEmployees;
+	
+	public Company() {
+		super();
+	}
+
+	public Company(String name, Double anualIncome, Integer numberOfEmployees) {
+		super(name, anualIncome);
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	public int getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+
+	public void setNumberOfEmployees(Integer numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	@Override
+	public double tax() {
+		if(numberOfEmployees <= 10) {
+			return getAnualIncome() * 0.16;
+		}else {
+			return getAnualIncome() * 0.14;
+		}
+	}
 
 }
--- src/entities/Individual.java
+++ src/entities/Individual.java
@@ -1,5 +1,33 @@
 package entities;
 
 public class Individual extends TaxPayer{
+	
+	public Double healthExpenditures;
+	
+	public Individual(){
+		super();
+	}
+	
+	public Individual(String name, Double anualIncome, Double healthExpenditures) {
+		super(name, anualIncome);
+		this.healthExpenditures = healthExpenditures;
+	}
+	
+	public Double getHeathExpenditures() {
+		return healthExpenditures;
+	}
+
+	public void setHeathExpenditures(Double healthExpenditures) {
+		this.healthExpenditures = healthExpenditures;
+	}
+
+	@Override
+	public double tax() {
+		if(anualIncome <= 20000.00) {
+			return getAnualIncome() * 0.15 - healthExpenditures * 0.5;
+		}else {
+			return getAnualIncome() * 0.25 - healthExpenditures * 0.5;
+		}
+	}
 
 }
